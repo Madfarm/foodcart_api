ea6e39d481bf0794144192ccdf71d9ac43d95d06
AnthonyScott <anthony.scott1005@gmail.com>
2023-12-19 22:38:19 -0500

    Add model for MenuItem

--- base/models.py
+++ base/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    description = models.CharField(max_length=100)
+    image = models.CharField(max_length=1000)
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
