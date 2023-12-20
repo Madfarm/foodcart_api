48811a18f8f250b01f76952b477a4dffdea9e4ec
AnthonyScott <anthony.scott1005@gmail.com>
2023-12-19 23:52:45 -0500

    Add documentation to models

--- base/models.py
+++ base/models.py
@@ -1,10 +1,26 @@
 from django.db import models
 
+
+
+
+
+# Note to future Anthony, migrations have not been made
+
+
+
 # Create your models here.
 class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     created = models.DateTimeField(auto_now_add=True)
+    cart = models.ForeignKey('Cart', related_name='menu', on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
+    
+class Cart(models.Model):
+    name = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.name)
