6d0d22aa54748198165dc4fb6221bdf8a5ec8ba2
AnthonyScott <anthony.scott1005@gmail.com>
2023-12-19 22:49:16 -0500

    Add serializer for menu items

--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from base.models import MenuItem
+from .serializers import MenuItemSerializer
 
 
 @api_view(['GET'])
