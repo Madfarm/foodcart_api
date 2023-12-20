c756e7a523ee0f2274f6662b2235aaaf91e2ba69
AnthonyScott <anthony.scott1005@gmail.com>
2023-12-19 22:46:39 -0500

    Add initial migrations

--- base/models.py
+++ base/models.py
@@ -4,9 +4,7 @@ from django.db import models
 class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField()
-    description = models.CharField(max_length=100)
-    image = models.CharField(max_length=1000)
-    category = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
